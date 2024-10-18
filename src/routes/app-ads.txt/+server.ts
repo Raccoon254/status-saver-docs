830a628abdec21951e844d81346e8de340c61067
Raccoon254 <tomsteve187@gmail.com>
2024-10-18 14:14:38 +0300

    complete test

--- src/routes/app-ads.txt/+server.ts
+++ src/routes/app-ads.txt/+server.ts
@@ -1,8 +1,6 @@
 import { json } from '@sveltejs/kit';
 
-export async function GET() {
+export function GET() {
   const content = "google.com, pub-4843196762316516, DIRECT, f08c47fec0942fa0"
-  return new Response(content, {
-    headers: { 'Content-Type': 'text/plain' }
-  });
+  return json(content);
 }
--- src/routes/test/+server.ts
+++ src/routes/test/+server.ts
@@ -2,11 +2,5 @@ import { json } from '@sveltejs/kit';
 
 export function GET() {
 	const number = Math.floor(Math.random() * 6) + 1;
-
-	return new Response(number, {
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
 	return json(number);
 }
