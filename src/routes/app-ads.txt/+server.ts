00acca4f64301985c4dba44295c84734f0cefd55
Raccoon254 <tomsteve187@gmail.com>
2024-10-18 13:45:45 +0300

    --stand alone endpoint to test

--- src/routes/app-ads.txt/+server.ts
+++ src/routes/app-ads.txt/+server.ts
@@ -1,9 +1,7 @@
-// src/routes/app-ads.txt/+server.ts
 import { json } from '@sveltejs/kit';
-import { readFile } from 'fs/promises';
 
 export async function GET() {
-  const content = await readFile('static/app-ads.txt', 'utf8');
+  const content = "google.com, pub-4843196762316516, DIRECT, f08c47fec0942fa0"
   return new Response(content, {
     headers: { 'Content-Type': 'text/plain' }
   });
